3e67115e44b71859c825ba30ea1fb7b3e1415d59
KaitlynKeil <kaitly.keil@students.olin.edu>
2018-02-01 16:55:52 -0500

    Made it read our file

--- scripts/random_rec.py
+++ scripts/random_rec.py
@@ -1,2 +1,18 @@
 """This script will choose a random book from primary_list.csv when the bear is asked
 for a recommendation."""
+
+import pandas as pd
+import os
+
+def readBooksDatabase(database):
+	""" Given a database in CSV format,
+	loads it into a pandas dataframe.
+	"""
+	books = pd.read_csv(database)
+	return books
+
+
+if __name__ == '__main__':
+	os.chdir('../docs')
+	books = readBooksDatabase('primary_list.csv')
+	print(books)
