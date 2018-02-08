694d25816d662b71f7bf884ce357989199418383
songbird175 <celina.bekins@students.olin.edu>
2018-02-07 23:26:29 -0500

    Inbound SMS body prints & echoes back

--- scripts/receive_sms.py
+++ scripts/receive_sms.py
@@ -5,24 +5,30 @@ Receives SMS messages
 # /usr/bin/env python
 # Download the twilio-python library from twilio.com/docs/libraries/python
 from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
+import os
+from twilio.twiml.messaging_response import MessagingResponse, Message
+from twilio.rest import Client
 
 app = Flask(__name__)
 
-@app.route("/sms", methods=['GET', 'POST'])
-def sms_ahoy_reply():
-    """Respond to incoming messages with a friendly SMS."""
-    # Start our response
-    resp = MessagingResponse()
+ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
-    # Add a message
-    resp.message("Ahoy! Thanks so much for your message.")
+@app.route("/sms", methods=['GET', 'POST'])
+def inbound_sms():
+    body = request.values.get('Body', None)
+
+    resp = MessagingResponse()
+    print(body) #prints out the message body!!!
 
     return str(resp)
+    
 
-@app.route('/')
-def hello_world():
-    return "Hello World"
+# @app.route('/')
+# def hello_world():
+#     return "Hello World"
 
 if __name__ == "__main__":
     app.run(debug=True)
